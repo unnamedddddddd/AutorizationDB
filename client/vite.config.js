3e5a32c72545ef6ab6044224fc2d7312d98dfe52
Denis <deniskamaldinov85@gmail.com>
2026-01-09 19:56:12 +0500

    Изменил vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base: '/',  
+  
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
+  
   server: {
     proxy: {
       '/api': {
